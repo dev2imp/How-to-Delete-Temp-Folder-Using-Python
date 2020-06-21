960472674dcbf1a22772d8ea81540161dd8238ab
osman inci <38284558+dev2imp@users.noreply.github.com>
2020-06-21 17:47:41 +0300

    Update DeleteTemp.py

--- DeleteTemp.py
+++ DeleteTemp.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-thelink='C:/Users/devos/AppData/Local/Temp'
-for filename in os.listdir(thelink):
+path='C:/Users/***/AppData/Local/Temp'
+for filename in os.listdir(path):
     print(filename)
     if filename!="cam_clean.bat":#cam_clean.bat is the file doesnt appear and doesnt let me to delet
         shutil.rmtree(thelink+"/"+filename)
